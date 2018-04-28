8539667c2a1a9c3d26e414ec816e4ca1c2454c38
zhangyw <zhangyw@huadong.net>
2018-04-28 15:53:50 +0800

    first

---
+++ src/main/java/net/huadong/tech/echarts/code/CoordinateSystem.java
@@ -0,0 +1,11 @@
+package net.huadong.tech.echarts.code;
+
+/**
+ * 坐标系
+ *
+ * @author liuzh
+ * @since 2016-02-28 10:20
+ */
+public enum CoordinateSystem {
+    cartesian2d, polar, geo
+}
---
+++ src/main/java/net/huadong/tech/echarts/code/EffectType.java
@@ -0,0 +1,11 @@
+package net.huadong.tech.echarts.code;
+
+/**
+ * 特效类型
+ *
+ * @author liuzh
+ * @since 2016-02-28 10:33
+ */
+public enum EffectType {
+    ripple //涟漪特效
+}
